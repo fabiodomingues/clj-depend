8d643dc565e4196058c8f519cfaebf42f4ea458f
Fabio Domingues <contato@fabiodomingues.com>
2024-04-08 20:15:20 -0300

    Version 0.10.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.fabiodomingues/clj-depend "0.10.0"
+(defproject com.fabiodomingues/clj-depend "0.10.1-SNAPSHOT"
   :description "A Clojure namespace dependency analyzer"
   :url "https://github.com/fabiodomingues/clj-depend"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
