91dae016ae8d0989307095282f46b9ed297c2fee
Fabio Domingues <contato@fabiodomingues.com>
2024-04-19 20:43:52 -0300

    Version 0.11.1-SNAPSHOT

--- lein-clj-depend/project.clj
+++ lein-clj-depend/project.clj
@@ -1,9 +1,9 @@
-(defproject com.fabiodomingues/lein-clj-depend "0.11.0"
+(defproject com.fabiodomingues/lein-clj-depend "0.11.1-SNAPSHOT"
   :description "Lein plugin to run clj-depend"
   :url "https://github.com/fabiodomingues/clj-depend"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true
-  :dependencies [[com.fabiodomingues/clj-depend "0.11.0"]]
+  :dependencies [[com.fabiodomingues/clj-depend "0.11.1-SNAPSHOT"]]
   :deploy-repositories [["releases" {:url "https://repo.clojars.org"
                                      :creds :gpg}]])
